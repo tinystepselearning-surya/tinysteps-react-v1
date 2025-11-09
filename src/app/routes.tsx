311881f581b02e5d50c35769d15f1dbf00a59d1b
suryaphonics <suryaphonics@gmail.com>
2025-11-09 17:59:12 +0530

    fix(ci): add src/app/index.tsx to resolve import ambiguity between src/app.tsx and src/app/ directory

---
+++ src/app/routes.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../app';
+import LoginPage from '../pages/LoginPage';
+import UnauthorizedPage from '../pages/UnauthorizedPage';
+import NotFoundPage from '../pages/NotFoundPage';
+
+// Dashboards (to be created)
+import AdminDashboard from '../pages/admin/AdminDashboard';
+import TeacherDashboard from '../pages/teacher/TeacherDashboard';
+import ParentDashboard from '../pages/parent/ParentDashboard';
+import LPDashboard from '../pages/lp/LPDashboard';
+import KidDashboard from '../pages/kid/KidDashboard';
+
+// Layout
+import Header from '../components/common/Header';
+import RoleGate from '../components/common/RoleGate';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'unauthorized',
+        element: <UnauthorizedPage />,
+      },
+      {
+        path: 'admin',
+        element: <RoleGate allowedRoles={['admin']} />,
+        children: [
+          {
+            path: '',
+            element: <AdminDashboard />,
+          },
+        ],
+      },
+      {
+        path: 'teacher',
+        element: <RoleGate allowedRoles={['teacher']} />,
+        children: [
+          {
+            path: '',
+            element: <TeacherDashboard />,
+          },
+        ],
+      },
+      {
+        path: 'parent',
+        element: <RoleGate allowedRoles={['parent']} />,
+        children: [
+          {
+            path: '',
+            element: <ParentDashboard />,
+          },
+        ],
+      },
+      {
+        path: 'learningpartner',
+        element: <RoleGate allowedRoles={['learningPartner']} />,
+        children: [
+          {
+            path: '',
+            element: <LPDashboard />,
+          },
+        ],
+      },
+      {
+        path: 'kid',
+        element: <RoleGate allowedRoles={['kid']} />,
+        children: [
+          {
+            path: '',
+            element: <KidDashboard />,
+          },
+        ],
+      },
+    ],
+  },
+]);
