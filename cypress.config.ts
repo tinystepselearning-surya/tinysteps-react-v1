1d12c46ab4eef43d80a48d773c20058df03b111f
Tiny Steps <tinysteps@Tinys-MacBook-Air.local>
2025-12-14 01:20:55 +0530

    chore: remove remaining cypress config references

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  projectId: '1go25p',
-  e2e: {
-    setupNodeEvents(on: any, config: any) {
-      // implement node event listeners here
-    },
-    specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-  },
-};
