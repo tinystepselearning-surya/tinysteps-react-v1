8b2e570d1d0c6ac6ca949f588a390841487d0ccd
Surya Prakash <tinystepselearning@gmail.com>
2025-12-17 13:09:50 +0530

    chore(teacher): remove worksheet generator completely

--- src/pages/teacher/WorksheetGeneratorPage.tsx
+++
@@ -1,7 +0,0 @@
-// WorksheetGeneratorPage has been removed from production routing.
-// Kept minimal stub to avoid accidental imports until fully deleted.
-import React from 'react';
-
-export default function WorksheetGeneratorPage() {
-  return null;
-}
