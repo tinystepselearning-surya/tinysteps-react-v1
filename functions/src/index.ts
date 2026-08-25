4bbf9a489d6277957b514ff9c336ffc2821012ee
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-08-26 01:17:21 +0530

    B6 Brick 7D2A export session-create certification callable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -41,6 +41,7 @@ export { auditParentPaymentBackfillDryRun } from "./parentPaymentBackfillDryRun"
 export { applyParentPaymentBackfillForSafeParents } from "./parentPaymentBackfillWriteMode";
 export { runFinanceReconciliationAudit, runFinanceReconciliationAuditDaily } from "./financeReconciliationReport";
 export { auditTeacherEarningsCanonicalCoverage } from "./auditTeacherEarningsCanonicalCoverage";
+export { certifyTeacherEarningsSessionCreateFastPath } from "./certifyTeacherEarningsSessionCreateFastPath";
 export { prepareTeacherFinanceAnalyticsRollups } from "./prepareTeacherFinanceAnalyticsRollups";
 export { reconcileParentPaymentsMonthReadModels } from "./reconcileParentPaymentsMonthReadModels";
 export { recordLegacyFallbackUsage } from "./legacyFallbackMetrics";
