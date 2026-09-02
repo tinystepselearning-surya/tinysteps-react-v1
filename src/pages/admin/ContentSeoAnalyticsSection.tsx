a2d5fca9627b48243f646f46fcc5f89fd634c45f
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-02 14:48:00 +0530

    fix(analytics): reset Brick 7 safely when reporting range changes

--- src/pages/admin/ContentSeoAnalyticsSection.tsx
+++ src/pages/admin/ContentSeoAnalyticsSection.tsx
@@ -1,2 +1,13 @@
-export { default } from './ContentSeoAnalyticsSectionV2';
+import React from 'react';
+import ContentSeoAnalyticsSectionV2, {
+  type ContentSeoAnalyticsSectionProps,
+} from './ContentSeoAnalyticsSectionV2';
+
 export type { ContentSeoAnalyticsSectionProps } from './ContentSeoAnalyticsSectionV2';
+
+export default function ContentSeoAnalyticsSection(
+  props: ContentSeoAnalyticsSectionProps,
+): JSX.Element {
+  const rangeKey = `${props.startDateKey || ''}:${props.endDateKey || ''}`;
+  return <ContentSeoAnalyticsSectionV2 key={rangeKey} {...props} />;
+}
