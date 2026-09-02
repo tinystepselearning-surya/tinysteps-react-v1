fbb72336f5c0d62a1cc1b3160a53b4de19bdfdbd
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-02 14:39:48 +0530

    feat(analytics): wire Brick 7 Content SEO section

--- src/pages/admin/LeadSourceAnalysis.tsx
+++ src/pages/admin/LeadSourceAnalysis.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LeadSourceAnalysisV5, { type LeadSourceAnalysisProps } from './LeadSourceAnalysisV5';
 import ExternalTrafficAnalyticsSection from './ExternalTrafficAnalyticsSection';
+import ContentSeoAnalyticsSection from './ContentSeoAnalyticsSection';
 
 export type { LeadSourceAnalysisProps } from './LeadSourceAnalysisV5';
 
@@ -9,10 +10,16 @@ export default function LeadSourceAnalysis(props: LeadSourceAnalysisProps): JSX.
     <div className="space-y-4">
       <LeadSourceAnalysisV5 {...props} />
       {props.showAttribution !== false ? (
-        <ExternalTrafficAnalyticsSection
-          startDateKey={props.startDateKey}
-          endDateKey={props.endDateKey}
-        />
+        <>
+          <ExternalTrafficAnalyticsSection
+            startDateKey={props.startDateKey}
+            endDateKey={props.endDateKey}
+          />
+          <ContentSeoAnalyticsSection
+            startDateKey={props.startDateKey}
+            endDateKey={props.endDateKey}
+          />
+        </>
       ) : null}
     </div>
   );
