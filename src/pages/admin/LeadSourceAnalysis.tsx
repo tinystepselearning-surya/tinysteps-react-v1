ef291c262fce5f85fdd3dc6e6a7e283be887f91c
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-02 08:41:00 +0530

    feat(analytics): attach external traffic section to acquisition

--- src/pages/admin/LeadSourceAnalysis.tsx
+++ src/pages/admin/LeadSourceAnalysis.tsx
@@ -1,2 +1,19 @@
-export { default } from './LeadSourceAnalysisV5';
+import React from 'react';
+import LeadSourceAnalysisV5, { type LeadSourceAnalysisProps } from './LeadSourceAnalysisV5';
+import ExternalTrafficAnalyticsSection from './ExternalTrafficAnalyticsSection';
+
 export type { LeadSourceAnalysisProps } from './LeadSourceAnalysisV5';
+
+export default function LeadSourceAnalysis(props: LeadSourceAnalysisProps): JSX.Element {
+  return (
+    <div className="space-y-4">
+      <LeadSourceAnalysisV5 {...props} />
+      {props.showAttribution !== false ? (
+        <ExternalTrafficAnalyticsSection
+          startDateKey={props.startDateKey}
+          endDateKey={props.endDateKey}
+        />
+      ) : null}
+    </div>
+  );
+}
