93129dc541554c63d75c3f35dd67759fe197fd1a
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-06 19:08:48 +0530

    Blog 74: promote clean video feedback URL

--- src/lib/blogIndexingPolicy.js
+++ src/lib/blogIndexingPolicy.js
@@ -49,6 +49,7 @@ const QUALITY_PROMOTED_PUBLIC_SLUGS = new Set([
   'speaking-structure',
   'speaking-visual-aids',
   'speaking-debate-starters',
+  'speaking-video-feedback',
 ]);
 
 const RENAMED_WEEKLY_PUBLIC_SLUGS = new Set(LEGACY_WEEK_PUBLIC_SLUGS);
