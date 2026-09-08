eef8e3ec59271a49cbb369b6e79cbe5050cadd5e
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-08 15:02:14 +0530

    Brick 1: derive public contact from semantic facts

--- src/constants/publicContact.ts
+++ src/constants/publicContact.ts
@@ -1,6 +1,9 @@
-export const PUBLIC_CONTACT_EMAIL = 'RavaliPriyaVannala@tinystepslearning.com';
+import { SEMANTIC_FACTS } from '../config/semanticFacts';
+
+export const PUBLIC_CONTACT_EMAIL = SEMANTIC_FACTS.contact.email;
 export const PUBLIC_CONTACT_MAILTO = `mailto:${PUBLIC_CONTACT_EMAIL}`;
-export const PUBLIC_WHATSAPP_NUMBER = '919618398383';
+export const PUBLIC_WHATSAPP_NUMBER = SEMANTIC_FACTS.contact.whatsappNumber;
+export const PUBLIC_CONTACT_TELEPHONE = SEMANTIC_FACTS.contact.telephoneDisplay;
 
 export const buildPublicWhatsAppUrl = (message: string) =>
   `https://wa.me/${PUBLIC_WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
