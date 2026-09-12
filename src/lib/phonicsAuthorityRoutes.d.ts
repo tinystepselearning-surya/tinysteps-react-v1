f53a984306a8197149cd95bc7409b70b9c0d6a6a
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-12 22:06:16 +0530

    fix: type phonics authority route registry

---
+++ src/lib/phonicsAuthorityRoutes.d.ts
@@ -0,0 +1,5 @@
+export const PHONICS_AUTHORITY_SLUGS: readonly string[];
+export const PHONICS_AUTHORITY_ROUTES: readonly string[];
+
+export const PHONICS_34_AUTHORITY_SLUGS: readonly string[];
+export const PHONICS_34_AUTHORITY_ROUTES: readonly string[];
