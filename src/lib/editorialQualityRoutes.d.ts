7ea5ad113bdfb5297ba45382fbaa84dd2825d35c
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-12 22:07:02 +0530

    fix: type editorial authority route registry

---
+++ src/lib/editorialQualityRoutes.d.ts
@@ -0,0 +1,8 @@
+export const PARENT_COMMUNICATION_17_AUTHORITY_SLUGS: readonly string[];
+export const PARENT_COMMUNICATION_17_AUTHORITY_ROUTES: readonly string[];
+
+export const QUALITY_AUTHORITY_SLUGS: readonly string[];
+export const QUALITY_AUTHORITY_ROUTES: readonly string[];
+
+export const QUALITY_51_AUTHORITY_SLUGS: readonly string[];
+export const QUALITY_51_AUTHORITY_ROUTES: readonly string[];
