8feac5367c693f15323bc80fb04ead057a583e7f
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-20 01:44:59 +0530

    AVS: export first-time baseline callable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -77,6 +77,7 @@ export { createMakeupSessionFromCredit } from "./createMakeupSessionFromCredit";
 export { saveTeacherSessionProgress, adminAttendanceCorrection } from "./saveTeacherSessionProgress";
 export { runAttendanceValidationLatestCheck } from "./attendanceValidation/latestCheckCallable";
 export { forceRefreshAttendanceValidationEvidence } from "./attendanceValidation/forceFreshEvidenceCallable";
+export { runAttendanceValidationFirstTimeBaseline } from "./attendanceValidation/firstTimeBaselineCallable";
 export { onAdminAttendanceCorrectionCompletionBridge } from "./adminAttendanceCorrectionCompletionBridge";
 export {
   prepareAdminAttendanceCorrectionTeacherPayDecision,
