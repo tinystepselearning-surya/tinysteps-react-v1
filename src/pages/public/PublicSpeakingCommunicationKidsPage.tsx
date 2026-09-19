8662fbcad54f5e99c8dcd1a6f2cef88df13c0927
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-19 12:23:47 +0530

    refactor(speaking-seo): retire duplicate speaking landing component

--- src/pages/public/PublicSpeakingCommunicationKidsPage.tsx
+++
@@ -1,5 +0,0 @@
-import SubjectLandingPage from '../../components/public/SubjectLandingPage';
-
-export default function PublicSpeakingCommunicationKidsPage() {
-  return <SubjectLandingPage subject="speaking" />;
-}
