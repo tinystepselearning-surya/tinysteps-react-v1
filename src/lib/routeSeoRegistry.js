794c242978204d35d04eb957f1120f0d044dcacb
tinystepselearning-surya <tinystepselearning@gmail.com>
2026-09-27 03:05:43 +0530

    perf(grammar): keep SEO registry lightweight

--- src/lib/routeSeoRegistry.js
+++ src/lib/routeSeoRegistry.js
@@ -1,6 +1,6 @@
 import { PUBLIC_COURSE_PAGE_CONFIGS } from './publicCoursePages.js';
 import { PHONICS_PUBLISHED_RESOURCE_SEO } from './phonicsPublicationRegistry.js';
-import { GRAMMAR_PROGRAMMATIC_RESOURCE_SEO } from './grammarProgrammaticRegistry.js';
+import { GRAMMAR_PROGRAMMATIC_RESOURCE_SEO } from './grammarProgrammaticSeoManifest.js';
 
 export const ROUTE_SEO_REGISTRY = {
   ...PHONICS_PUBLISHED_RESOURCE_SEO,
